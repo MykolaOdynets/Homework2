1b272ae86321b36a52717074fa9ad8161d697dd3
MykolaOdynets <44775020+MykolaOdynets@users.noreply.github.com>
2018-11-23 15:51:16 +0200

    Add files via upload

---
+++ Page.java
@@ -0,0 +1,20 @@
+package Site1;
+
+public class Page {
+	//Кожна веб сторінка (page) має 3 характеристики - title і url, htmlContent.
+	int num;
+	String title;
+	String url;
+	String htmlCotnent;
+	 void htmlBuilder(String[] args) {	
+	 	System.out.println("Я виводжу метод htmlBuilder для сторінки " + " "+ title +" "+ url);
+	 }
+	 void refresh() {	 }
+	 
+	 void load() {	 }
+	 
+	 void get_html_Chrome() { }
+	 void get_html_Firefox() { }
+	 
+	 
+}
